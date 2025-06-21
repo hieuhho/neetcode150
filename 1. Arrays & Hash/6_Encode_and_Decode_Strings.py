88b18885e8cee505638b2c1dc3a47eedf93f5a15
Hieu <hieuhho.dev@gmail.com>
2025-06-20 20:27:32 -0700

    solved problem 6

--- 1. Arrays & Hash/6_Encode_and_Decode_Strings.py
+++ 1. Arrays & Hash/6_Encode_and_Decode_Strings.py
@@ -22,5 +22,32 @@
 #     0 <= strs.length < 100
 #     0 <= strs[i].length < 200
 #     strs[i] contains only UTF-8 characters.
+
 class Solution:
     def encode(self, strs: List[str]) -> str:
+        res = ""
+        for i in strs:
+            res += str(len(i)) + "#" + i
+        return res
+    
+    def decode(self, s: str) -> List[str]:
+        ans = []
+        # set starting index
+        i = 0
+        while i < len(s):
+            # set second index
+            j = i
+            # move the second index to till word start
+            while s[j] != "#":
+                j += 1
+            # the word length is between starting & finishing
+            word_length = int(s[i:j])
+            # move first index to where word start
+            i = j + 1
+            # second index is the start + word_length
+            j = i + word_length
+            ans.append(s[i:j])
+            # move the first index to the next word
+            i = j
+        return ans
+            
