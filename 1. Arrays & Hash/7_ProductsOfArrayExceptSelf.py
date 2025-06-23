d43b6e1d51767afc16da89de91a2dd3376580ec5
Hieu <hieuhho.dev@gmail.com>
2025-06-22 20:44:38 -0700

    solved problem 7

--- 1. Arrays & Hash/7_ProductsOfArrayExceptSelf.py
+++ 1. Arrays & Hash/7_ProductsOfArrayExceptSelf.py
@@ -26,3 +26,13 @@
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [1] * len(nums)
+        before = 1
+        after = 1
+        for i in range(len(nums)):
+            ans[i] = before
+            before *= nums[i]
+        for i in range(len(nums) -1, -1, -1):
+            ans[i] *= after
+            after *= nums[i]
+        return ans
