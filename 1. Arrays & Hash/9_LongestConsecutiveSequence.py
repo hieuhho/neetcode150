efbf36892eebcadfee170446ce9c0eef797fd612
Hieu <hieuhho.dev@gmail.com>
2025-06-23 20:47:19 -0700

    solved problem 9

--- 1. Arrays & Hash/9_LongestConsecutiveSequence.py
+++ 1. Arrays & Hash/9_LongestConsecutiveSequence.py
@@ -28,3 +28,12 @@
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        max_len = 0
+        for i in nums:
+            if i + 1 not in nums_set:
+                length = 1
+                while i + length in nums_set:
+                    length += 1
+                max_len = max(max_len, length)
+        return max_len
