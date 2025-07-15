4e2ac6170d472d336e682ddfbc88293f8cb8557f
Hieu <hieuhho.dev@gmail.com>
2025-07-14 21:01:33 -0700

    add non optimal solution

--- 1. Arrays & Hash/7_ProductsOfArrayExceptSelf.py
+++ 1. Arrays & Hash/7_ProductsOfArrayExceptSelf.py
@@ -36,3 +36,21 @@ class Solution:
             ans[i] *= after
             after *= nums[i]
         return ans
+
+
+## first approach
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        pre = [1] * n
+        post = [1] * n
+        ans = [1] * n
+        for i in range(1, n):
+            pre[i] = nums[i - 1] * pre[i - 1] 
+        for i in range(n - 2, -1, -1):
+            post[i] = nums[i + 1] * post[i + 1]
+        print(pre)
+        print(post)
+        for i in range(n):
+            ans[i] = pre[i] * post[i]
+        return ans
