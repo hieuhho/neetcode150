f911af37e0ff91c5fdd96d90757a0941a7e840fd
Hieu <hieuhho.dev@gmail.com>
2025-07-21 20:40:32 -0700

    solved 2.1.3

--- 2.1 Two Pointers/3_3Sum.py
+++ 2.1 Two Pointers/3_3Sum.py
@@ -40,4 +40,29 @@
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+        nums.sort()
+        ans = []
+        n = len(nums)
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = -nums[i]
+            j = i + 1
+            k = n - 1
+
+            while j < k:
+                two_sum = nums[j] + nums[k]
+                if two_sum == target:
+                    ans.append[nums[i], nums[j], nums[k]]
+                    j += 1
+                    k -= 1
+
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+                elif two_sum < target:
+                    j += 1
+                else:
+                    k -= 1
+        return ans
