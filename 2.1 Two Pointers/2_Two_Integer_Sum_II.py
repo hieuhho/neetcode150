c3654df3eea1380da8284acc614f00bddf13cf77
Hieu <hieuhho.dev@gmail.com>
2025-07-21 20:13:47 -0700

    solved 2.1

--- 2.1 Two Pointers/2_Two_Integer_Sum_II.py
+++ 2.1 Two Pointers/2_Two_Integer_Sum_II.py
@@ -26,3 +26,13 @@
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        start = 0
+        end = len(numbers) - 1
+        while start < end:
+            current_sum = numbers[start] + numbers[end]
+            if current_sum == target:
+                return [start + 1, end + 1]
+            if current_sum < target:
+                start += 1
+            else:
+                end -= 1
