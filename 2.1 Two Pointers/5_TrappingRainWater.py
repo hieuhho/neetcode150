a66a5896a9cfe6bfb83a9061ccceb57f8311fac1
Hieu <hieuhho.dev@gmail.com>
2025-07-23 21:40:03 -0700

    solve 2.1.5

--- 2.1 Two Pointers/5_TrappingRainWater.py
+++ 2.1 Two Pointers/5_TrappingRainWater.py
@@ -17,4 +17,23 @@
 
 class Solution:
     def trap(self, height: List[int]) -> int:
+        n = len(height)
+        ans_arr = [0] * n
+        current_max = 0
+        left_max = [0] * n
+        right_max = [0] * n
+
+        current_max = 0
+        for i in range(n):
+            left_max[i] = current_max
+            current_max = max(current_max, height[i])
         
+        current_max = 0
+        for i in range(n - 1, -1, -1):
+            right_max[i] = current_max
+            current_max = max(current_max, height[i])
+
+        for i in range(n):
+            water = min(left_max[i], right_max[i]) - height[i]
+            ans_arr[i] = 0 if water < 0 else water
+        return sum(ans_arr)
