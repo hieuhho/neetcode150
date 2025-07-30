755ee8473ce6e36d76ca4f76376c04cc68ce8457
Hieu <hieuhho.dev@gmail.com>
2025-07-29 21:16:53 -0700

    solved 2.2.2

--- 2.2 Stack/2_MinimumStack.py
+++ 2.2 Stack/2_MinimumStack.py
@@ -35,16 +35,23 @@
 class MinStack:
 
     def __init__(self):
-        
+        self.stack = []
+        self.min_val = []
+
 
     def push(self, val: int) -> None:
-        
+        self.stack.append(val)
+        min_val = min(self.min_val[-1] if self.min_val else val, val)
+        self.min_val.append(min_val)
+
 
     def pop(self) -> None:
-        
+        self.stack.pop()
+        self.min_val.pop()
+
 
     def top(self) -> int:
-        
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        
+        return self.min_val[-1]
