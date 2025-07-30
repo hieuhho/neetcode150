f61bd1a49a7da8605bbe9e7017743b1fa67b2eb1
Hieu <hieuhho.dev@gmail.com>
2025-07-29 18:51:28 -0700

    solved 2.2.1

--- 2.2 Stack/1_ValidParentheses.py
+++ 2.2 Stack/1_ValidParentheses.py
@@ -36,3 +36,33 @@
 #     1 <= s.length <= 1000
 class Solution:
     def isValid(self, s: str) -> bool:
+        # create a dict of parentheses pairs
+        # create arr
+        # loop through s
+        # 1) if first i is closed, return false
+        # 2) if bracket is close, check arr
+        # 2.1) if top of arr is open & matching
+        # 2.1.a) remove top of arr, continue
+        # 2.2) if top of arr is open & NOT matching
+        # 2.2.a) return false
+        # 3) if bracket is open, add to arr
+        # return if arr is empty or not
+        stack = []
+        open_bracket_dict = {
+            "(": ")",
+            "{": "}",
+            "[": "]"
+        }
+        close_bracket_dict = dict([(value, key) for key, value in open_bracket_dict.items()])
+    
+        for i in s:
+            if i in close_bracket_dict:
+                if stack[-1] != close_bracket_dict[i]:
+                    return False
+                else:
+                    stack.pop()
+            else:
+                stack.append(i)
+        return not stack
+
+
