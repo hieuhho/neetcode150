e625527a415e32b8b65643af22fa7f0b5ff75173
Hieu <hieuhho.dev@gmail.com>
2025-07-30 21:05:20 -0700

    solved 2.2.3

--- 2.2 Stack/3_EvaluateReversePolishNotation.py
+++ 2.2 Stack/3_EvaluateReversePolishNotation.py
@@ -52,3 +52,23 @@ class Solution:
         return ans[0]
 
     # this fails with tokens=["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        ans = []
+        for i in tokens:
+            if i == "+":
+                ans.append(ans.pop() + ans.pop())
+            elif i == "-":
+                a = ans.pop()
+                b = ans.pop()
+                ans.append(b - a)
+            elif i == "*":
+                ans.append(ans.pop() * ans.pop())
+            elif i == "/":
+                a = ans.pop()
+                b = ans.pop()
+                ans.append(int(b / a))
+            else:
+                ans.append(int(i))
+        return ans[0]
