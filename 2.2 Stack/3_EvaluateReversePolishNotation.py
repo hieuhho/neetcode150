6355adb377be3cab6a174af93d969e602b7841bc
Hieu <hieuhho.dev@gmail.com>
2025-07-30 21:01:37 -0700

    naive solve for 2.2.3

--- 2.2 Stack/3_EvaluateReversePolishNotation.py
+++ 2.2 Stack/3_EvaluateReversePolishNotation.py
@@ -21,5 +21,34 @@
 
 #     1 <= tokens.length <= 1000.
 #     tokens[i] is "+", "-", "*", or "/", or a string representing an integer in the range [-100, 100].
+import operator
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        # make stack
+        # for i in tokens
+        # check i is alphanumeric
+        # append to stack
+        # else
+        # check if stack has num
+        # no -> return 0
+        # yes -> stack_num operator i
+        # add solution to stack
+        ops = {
+            '+' : operator.add,
+            '-' : operator.sub,
+            '*' : operator.mul,
+            '/' : lambda a, b: int(a / b) if a * b > 0 else -(-a // b)
+        }
+        ans = []
+        for i in tokens:
+            if i.isalnum():
+                ans.append(int(i))
+            else:
+                if ans:
+                    solved = ops[i](ans[-2], ans[-1])
+                    ans.pop()
+                    ans.pop()
+                    ans.append(solved)
+        return ans[0]
+
+    # this fails with tokens=["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
