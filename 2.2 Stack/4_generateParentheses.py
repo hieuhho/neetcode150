399384e25efe02f3600e3a5f3c472c23cad89ea3
Hieu <hieuhho.dev@gmail.com>
2025-07-31 20:53:35 -0700

    solved 2.2.4

--- 2.2 Stack/4_generateParentheses.py
+++ 2.2 Stack/4_generateParentheses.py
@@ -40,3 +40,29 @@
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        ans = []
+
+        # do a recursion
+        # check for number of open and close
+        # if open and close == n, add stack to ans, return
+        # if open < n, add open to stack, continue to recurse with open count + 1
+        # if close < open, add close to stack, continue to recurse with close count + 1
+
+        def recurse(open_count, close_count):
+            if open_count == close_count == n:
+                ans.append("".join(stack))
+                return
+
+            if open_count < n:
+                stack.append('(')
+                recurse(open_count + 1, close_count)
+                stack.pop()
+
+            if close_count < open_count:
+                stack.append(')')
+                recurse(open_count, close_count + 1)
+                stack.pop()
+
+        recurse(0, 0)
+        return ans
