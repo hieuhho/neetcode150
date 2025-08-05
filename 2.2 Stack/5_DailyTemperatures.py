5e59786fa5beb64411b82b5fcbf27ca97b003261
Hieu <hieuhho.dev@gmail.com>
2025-08-04 19:15:16 -0700

    solved 2.2.5

--- 2.2 Stack/5_DailyTemperatures.py
+++ 2.2 Stack/5_DailyTemperatures.py
@@ -24,3 +24,13 @@
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0] * n
+        stack = []
+        for idx, current_tempt in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < current_tempt:
+                ans[stack[-1]] = idx - stack[-1]
+                stack.pop()
+            stack.append(idx)
+        return ans
+
