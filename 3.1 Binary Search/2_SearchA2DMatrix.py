42ebea32cbfa1db6078af83d6d6a661ad1b84f40
Hieu <hieuhho.dev@gmail.com>
2025-08-11 14:06:36 -0700

    solved 3.1.2

--- 3.1 Binary Search/2_SearchA2DMatrix.py
+++ 3.1 Binary Search/2_SearchA2DMatrix.py
@@ -30,3 +30,14 @@
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        start = 0
+        end = len(matrix) - 1
+        while start <= end:
+            mid_row = start + ((end - start) // 2)
+            if matrix[mid_row][0] > target:
+                end = mid_row - 1
+            elif matrix[mid_row][-1] < target:
+                start = mid_row + 1
+            else:
+                return target in matrix[mid_row]
+        return False
