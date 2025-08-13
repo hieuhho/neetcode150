5dff21a4895a2364e2d3d7fb610fdae3212e31bf
Hieu <hieuhho.dev@gmail.com>
2025-08-12 20:51:02 -0700

    solved 3.1.3

--- 3.1 Binary Search/3_KokoEatingBanana.py
+++ 3.1 Binary Search/3_KokoEatingBanana.py
@@ -26,6 +26,21 @@
 #     piles.length <= h <= 1,000,000
 #     1 <= piles[i] <= 1,000,000,000
 
-
+import math
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # upper bound is max(piles)
+        # lower bound is 1
+        # binary search between 1 & upperbound
+        largest_k = max(piles)
+        lowest_k = 1
+        ans = largest_k
+        while lowest_k <= largest_k:
+            mid_k = lowest_k + ((largest_k - lowest_k) // 2)
+            hours = sum(math.ceil(pile / mid_k) for pile in piles)
+            if hours <= h:
+                ans = min(ans, mid_k)
+                largest_k = mid_k - 1
+            else:
+                lowest_k = mid_k + 1
+        return ans
