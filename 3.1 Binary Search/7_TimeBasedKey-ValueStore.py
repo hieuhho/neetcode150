40a39f1207154ef49d4645c216c076a8eab18a70
Hieu <hieuhho.dev@gmail.com>
2025-08-18 12:56:19 -0700

    solved 3.1.7

--- 3.1 Binary Search/7_TimeBasedKey-ValueStore.py
+++ 3.1 Binary Search/7_TimeBasedKey-ValueStore.py
@@ -38,10 +38,27 @@
 class TimeMap:
 
     def __init__(self):
-
+        self.hash_map = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
+        if not key in self.hash_map:
+            self.hash_map[key] = []
+        self.hash_map[key].append((timestamp, value))
 
 
     def get(self, key: str, timestamp: int) -> str:
-
+        if not self.hash_map.get(key):
+            return ""
+        l, r = 0, len(self.hash_map[key]) - 1
+        recent = ""
+        while l <= r:
+            m = l + (r - l) // 2
+            mid_ts, val = self.hash_map[key][m]
+            if mid_ts == timestamp:
+                return val
+            if mid_ts > timestamp:
+                r = m - 1
+            else:
+                recent = val
+                l = m + 1
+        return recent
