b4c3785b73072d17a38d5d9ea319d7b441c68b58
Hieu <hieuhho.dev@gmail.com>
2025-08-21 21:57:55 -0700

    solved 3.2.2

--- 3.2 Sliding Window/2_LongestSubstringWithoutRepeatingCharacters.py
+++ 3.2 Sliding Window/2_LongestSubstringWithoutRepeatingCharacters.py
@@ -26,3 +26,13 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        my_set = set()
+        ans = 0
+        l = 0
+        for r in range(len(s)):
+            while s[r] in my_set:
+                my_set.remove(s[l])
+                l += 1
+            my_set.add(s[r])
+            ans = max(ans, r - l + 1)
+        return ans
