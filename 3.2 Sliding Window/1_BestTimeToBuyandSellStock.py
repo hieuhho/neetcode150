9b0d2f1d0cd840a1b27804493393bbdcdd37bf19
Hieu <hieuhho.dev@gmail.com>
2025-08-21 21:39:13 -0700

    solved 3.2.1

--- 3.2 Sliding Window/1_BestTimeToBuyandSellStock.py
+++ 3.2 Sliding Window/1_BestTimeToBuyandSellStock.py
@@ -30,3 +30,13 @@
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        l, r = 0, 1
+        while r < len(prices):
+            if prices[l] > prices[r]:
+                l = r
+            else:
+                current_profit = prices[r] - prices[l]
+                profit = max(profit, current_profit)
+            r += 1
+        return profit
