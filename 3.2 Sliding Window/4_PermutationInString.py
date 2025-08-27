2ba958c9d44e4b5a270b168cb11db07b8a3ff0c6
Hieu <hieuhho.dev@gmail.com>
2025-08-26 20:45:29 -0700

    add 3.2.4

---
+++ 3.2 Sliding Window/4_PermutationInString.py
@@ -0,0 +1,28 @@
+# Permutation in String
+
+# You are given two strings s1 and s2.
+
+# Return true if s2 contains a permutation of s1, or false otherwise. That means if a permutation of s1 exists as a substring of s2, then return true.
+
+# Both strings only contain lowercase letters.
+
+# Example 1:
+
+# Input: s1 = "abc", s2 = "lecabee"
+
+# Output: true
+
+# Explanation: The substring "cab" is a permutation of "abc" and is present in "lecabee".
+
+# Example 2:
+
+# Input: s1 = "abc", s2 = "lecaabee"
+
+# Output: false
+
+# Constraints:
+
+#     1 <= s1.length, s2.length <= 1000
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
