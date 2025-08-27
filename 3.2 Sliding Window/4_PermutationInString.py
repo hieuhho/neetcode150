60dd6c0304ba994ea18b979eb93bb94e37776d48
Hieu <hieuhho.dev@gmail.com>
2025-08-26 21:21:17 -0700

    solved 3.2.4

--- 3.2 Sliding Window/4_PermutationInString.py
+++ 3.2 Sliding Window/4_PermutationInString.py
@@ -26,3 +26,20 @@
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_alphabet = [0] * 26
+        s2_alphabet  = [0] * 26
+        for letter in s1:
+            s1_alphabet[ord(letter) - ord("a")] += 1
+        l = 0
+        s1_len = len(s1)
+
+        for r in range(len(s2)):
+            s2_alphabet[ord(s2[r]) - ord("a")] += 1
+            if s1_len < (r - l + 1):
+                s2_alphabet[ord(s2[l]) - ord("a")] -= 1
+                l += 1
+
+            if s1_len == (r - l + 1):
+                if s2_alphabet == s1_alphabet:
+                    return True
+        return False
