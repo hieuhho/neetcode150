0ca62522fef684e73626403bc16bd63656959c0a
Hieu <hieuhho.dev@gmail.com>
2025-09-09 15:50:47 -0700

    solved 3.2.6 within constrains

--- 3.2 Sliding Window/6_SlidingWindowMaximum.py
+++ 3.2 Sliding Window/6_SlidingWindowMaximum.py
@@ -25,12 +25,27 @@
 #     -10,000 <= nums[i] <= 10,000
 #     1 <= k <= nums.length
 
+from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         ans = []
-        l = 0
-        for r in range(k, len(nums) + 1):
-            ans.append(max(nums[l:r]))
-            l += 1
+        q = deque()
+        l = r = 0
+
+        # keep the queue in decreasing order
+        while r < len(nums):
+            # pop smaller values
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            # resize window, q[0] is always the largest but we have to resize due to k
+            if q[0] < l:
+                q.popleft()
+
+            # when we have a valid k window, add q[0] then move l forward
+            if (r + 1) >= k:
+                ans.append(nums[q[0]])
+                l += 1
             r += 1
         return ans
