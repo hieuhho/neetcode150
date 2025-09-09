094da5599883a5f726a4c6dd092aacc1e07aa975
Hieu <hieuhho.dev@gmail.com>
2025-09-09 15:16:44 -0700

    solved 3.2.6 first try

--- 3.2 Sliding Window/6_SlidingWindowMaximum.py
+++ 3.2 Sliding Window/6_SlidingWindowMaximum.py
@@ -27,3 +27,10 @@
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        ans = []
+        l = 0
+        for r in range(k, len(nums) + 1):
+            ans.append(max(nums[l:r]))
+            l += 1
+            r += 1
+        return ans
