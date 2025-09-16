42157a27355ae340d97b62c225db6d9410ba61c5
Hieu <hieuhho.dev@gmail.com>
2025-09-15 17:18:51 -0700

    solved 6.3.5, add 6.3.6

--- 6.3 1-D Dynamic Programing/5_LongestPalindromicSubstring.py
+++ 6.3 1-D Dynamic Programing/5_LongestPalindromicSubstring.py
@@ -26,4 +26,26 @@
 #     s contains only digits and English letters.
 
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+        ans = ""
+        ans_len = 0
+        # check from middle
+        for i in range(len(s)):
+            # check odd palindrome
+            l = r = i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > ans_len:
+                    ans = s[l:r + 1]
+                    ans_len = r - l + 1
+                l -= 1
+                r += 1
+
+            # check even palindrome
+            l = i
+            r = i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > ans_len:
+                    ans = s[l:r + 1]
+                    ans_len = r - l + 1
+                l -= 1
+                r += 1
+        return ans
