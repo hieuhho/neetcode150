60560b4de51f4920f2b56226f08b4e026df13d25
Hieu <hieuhho.dev@gmail.com>
2025-09-17 15:04:55 -0700

    solved 6.3.9

--- 6.3 1-D Dynamic Programing/9_MaximumProductSubarray.py
+++ 6.3 1-D Dynamic Programing/9_MaximumProductSubarray.py
@@ -25,3 +25,12 @@
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        ans = max(nums)
+        current_max, current_min = 1, 1
+
+        for n in nums:
+            tmp_max = current_max
+            current_max = max(n * current_max, n * current_min, n)
+            current_min = min(n * tmp_max, n * current_min, n)
+            ans = max(ans, current_max, current_min)
+        return ans
