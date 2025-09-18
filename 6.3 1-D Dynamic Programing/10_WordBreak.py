7d2b965481c5928a44c2d5be59fdfd736e1d48ce
Hieu <hieuhho.dev@gmail.com>
2025-09-17 21:51:31 -0700

    solved 6.3.10 with comments

--- 6.3 1-D Dynamic Programing/10_WordBreak.py
+++ 6.3 1-D Dynamic Programing/10_WordBreak.py
@@ -35,3 +35,24 @@
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # dp[i] = True if substring s[i:] can be segmented into words from wordDict
+        dp = [False] * (len(s) + 1)
+
+        # base case: empty string at the end is always "breakable"
+        dp[len(s)] = True
+
+        # fill dp backwards: from the end of s to the start
+        for i in range(len(s) - 1, -1, -1):
+            for word in wordDict:
+                # check if the word fits starting at position i
+                if i + len(word) <= len(s) and s[i:i + len(word)] == word:
+                    # If "word" is a valid chunk at s[i:],
+                    # then the question becomes: can the rest, s[i+len(word):], also be segmented?
+                    # That is exactly dp[i + len(word)].
+                    dp[i] = dp[i + len(word)]
+                # if we already found a valid break, no need to check further
+                if dp[i]:
+                    break
+
+        # answer: can we segment the entire string s?
+        return dp[0]
