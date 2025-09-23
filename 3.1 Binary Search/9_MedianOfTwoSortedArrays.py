a3eb69768b18bdfcd860b2014843914382f27d2b
Hieu <hieuhho.dev@gmail.com>
2025-09-23 12:49:42 -0700

    solved 3.1.9

--- 3.1 Binary Search/9_MedianOfTwoSortedArrays.py
+++ 3.1 Binary Search/9_MedianOfTwoSortedArrays.py
@@ -30,3 +30,10 @@
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        merged = nums1 + nums2
+        merged.sort()
+
+        merged_len = len(merged)
+        if merged_len % 2 == 0:
+            return (merged[merged_len // 2 - 1] + merged[merged_len // 2]) / 2.0
+        return merged[merged_len // 2]
