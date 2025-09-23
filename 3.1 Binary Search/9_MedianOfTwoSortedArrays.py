d54d020b4905fbc58c9b93ce02fe7db4b6964866
Hieu <hieuhho.dev@gmail.com>
2025-09-23 16:01:11 -0700

    solved 3.1.9 binary search

--- 3.1 Binary Search/9_MedianOfTwoSortedArrays.py
+++ 3.1 Binary Search/9_MedianOfTwoSortedArrays.py
@@ -37,3 +37,38 @@ class Solution:
         if merged_len % 2 == 0:
             return (merged[merged_len // 2 - 1] + merged[merged_len // 2]) / 2.0
         return merged[merged_len // 2]
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        l1 = len(nums1)
+        l2 = len(nums2)
+
+        if l1 > l2:
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        max_len = l1 + l2
+        half_len = max_len // 2
+
+        l = 0
+        r = l1 - 1
+
+        while True:
+            i = (l + r) // 2 # nums1 middle
+            j = half_len - i - 2 # num2 left side
+
+            nums1_left = nums1[i] if i >= 0 else float("-infinity")
+            nums1_right = nums1[i + 1] if (i + 1) < len(nums1) else float("infinity")
+            nums2_left = nums2[j] if j >= 0 else float("-infinity")
+            nums2_right = nums2[j + 1] if (j + 1) < len(nums2) else float("infinity")
+
+            if nums1_left <= nums2_right and nums2_left <= nums1_right:
+                if max_len % 2:
+                    return min(nums1_right, nums2_right)
+                else:
+                    return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
+            elif nums1_left > nums2_right:
+                r = i - 1
+            else:
+                l = i + 1
+
+
