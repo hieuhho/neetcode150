1e3e8dad7f783cacd49101c4bb08ab862a8047ea
Hieu <hieuhho.dev@gmail.com>
2025-09-26 21:44:22 -0700

    solved 3.3.8

---
+++ 3.3 Linked List/8_FindTheDuplicateNumber.py
@@ -0,0 +1,35 @@
+# Find the Duplicate Number
+# Solved
+
+# You are given an array of integers nums containing n + 1 integers. Each integer in nums is in the range [1, n] inclusive.
+
+# Every integer appears exactly once, except for one integer which appears two or more times. Return the integer that appears more than once.
+
+# Example 1:
+
+# Input: nums = [1,2,3,2,2]
+
+# Output: 2
+
+# Example 2:
+
+# Input: nums = [1,2,3,4,4]
+
+# Output: 4
+
+# Follow-up: Can you solve the problem without modifying the array nums and using O(1)O(1) extra space?
+
+# Constraints:
+
+#     1 <= n <= 10000
+#     nums.length == n + 1
+#     1 <= nums[i] <= n
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        for i in nums:
+            idx = abs(i) - 1
+            if nums[idx] < 0:
+                return abs(i)
+            nums[idx] *= -1
+        return -1
