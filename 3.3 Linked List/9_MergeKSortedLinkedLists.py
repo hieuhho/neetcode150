250ade69c53b8e741d3bc722b8994e68e99d6864
Hieu <hieuhho.dev@gmail.com>
2025-09-30 11:31:57 -0700

    solved 3.3.9

--- 3.3 Linked List/9_MergeKSortedLinkedLists.py
+++ 3.3 Linked List/9_MergeKSortedLinkedLists.py
@@ -36,4 +36,24 @@
 #         self.next = next
 
 class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        tail.next = list1 or list2
+        return dummy.next
+
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0:
+            return None
+
+        for i in range(1, len(lists)):
+            lists[i] = self.mergeTwoLists(lists[i], lists[i -1])
+        return lists[-1]
