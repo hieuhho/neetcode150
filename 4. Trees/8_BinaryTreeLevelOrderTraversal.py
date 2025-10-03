f4a97c3c659f46e130a3c4d2a75224a1618c2228
Hieu <hieuhho.dev@gmail.com>
2025-10-03 12:32:49 -0700

    solved 4.8

--- 4. Trees/8_BinaryTreeLevelOrderTraversal.py
+++ 4. Trees/8_BinaryTreeLevelOrderTraversal.py
@@ -31,7 +31,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = []
 
+        queue = deque([root])
+        while queue:
+            queue_len = len(queue)
+            current_depth = []
+            for i in range(queue_len):
+                node = queue.popleft()
+                if node:
+                    current_depth.append(node.val)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+            if current_depth:
+                ans.append(current_depth)
+        return ans
