f29d08fa0b12fc2c1ad9c43a5f40c2ae03d3ee55
Hieu <hieuhho.dev@gmail.com>
2025-10-07 16:00:39 -0700

    solved 4.14

--- 4. Trees/14_BinaryTreeMaximumPathSum.py
+++ 4. Trees/14_BinaryTreeMaximumPathSum.py
@@ -36,4 +36,21 @@
 
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        # use a list so we don't have to use nonlocal inside dfs
+        ans = [root.val]
+        def dfs(node):
+            if not node:
+                return 0
 
+            left =  dfs(node.left)
+            right = dfs(node.right)
+            left_max = max(left, 0)
+            right_max = max(right, 0)
+
+            # update answer with sum of current node with left & right
+            ans[0] = max(ans[0], node.val + left_max + right_max)
+            # return max of current node sum
+            return node.val + max(left_max, right_max)
+
+        dfs(root)
+        return ans[0]
