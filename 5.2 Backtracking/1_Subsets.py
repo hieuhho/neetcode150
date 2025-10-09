496d6250e1c08d77ac0dd364ce25c626f9ccf01b
Hieu <hieuhho.dev@gmail.com>
2025-10-09 09:57:43 -0700

    solved 5.2.1

--- 5.2 Backtracking/1_Subsets.py
+++ 5.2 Backtracking/1_Subsets.py
@@ -23,3 +23,21 @@
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                ans.append(subset.copy())
+                return
+
+            # include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # ignore nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return ans
