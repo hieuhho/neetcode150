3ce9fc5a274e914a76ba51bc61ae78c77ac309eb
Hieu <hieuhho.dev@gmail.com>
2025-10-10 10:14:40 -0700

    solved 5.2.2

--- 5.2 Backtracking/2_CombinationSum.py
+++ 5.2 Backtracking/2_CombinationSum.py
@@ -43,3 +43,24 @@
 
 class Solution:
     def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        ans = []
+
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                return
+
+            if sum(subset) == target:
+                ans.append(subset.copy())
+                return
+
+            # include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # ignore nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return ans
