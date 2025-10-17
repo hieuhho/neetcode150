022bb5b216fccb980be4dd65e4be006b5a757563
Hieu <hieuhho.dev@gmail.com>
2025-10-17 15:05:33 -0700

    solved 6.1.3 maxheap

--- 6.1 Heap (Priority Queue)/3_KClosestPointsToOrigin.py
+++ 6.1 Heap (Priority Queue)/3_KClosestPointsToOrigin.py
@@ -44,3 +44,16 @@ class Solution:
             ans.append([x, y])
             k -= 1
         return ans
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distance = []
+        for x, y in points:
+            # use negative distance to use max-heap
+            dist = -((x ** 2) + (y ** 2))
+            heapq.heappush(distance, [dist, x, y])
+
+            if len(distance) > k:
+                heapq.heappop(distance)
+
+        return [[x, y] for dist, x, y in distance]
