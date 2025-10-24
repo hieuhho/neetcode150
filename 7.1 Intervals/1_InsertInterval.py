05f4821720145989c1e13c16afdb381b58344d71
Hieu <hieuhho.dev@gmail.com>
2025-10-24 16:33:58 -0700

    solved 7.1.1

--- 7.1 Intervals/1_InsertInterval.py
+++ 7.1 Intervals/1_InsertInterval.py
@@ -30,3 +30,18 @@
 
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort()
+        ans = [intervals[0]]
+
+        n = len(intervals)
+        for i in range(1, n):
+            start1, stop1 = ans[-1]
+            start2, stop2 = intervals[i]
+
+            if stop1 >= start2:
+                ans[-1] = [start1, max(stop1, stop2)]
+            else:
+                ans.append(intervals[i])
+        return ans
+
