97d607424eee1c622b59a7347fd174fd6e2051dd
Hieu <hieuhho.dev@gmail.com>
2025-10-24 16:53:03 -0700

    solved 7.1.1 more efficient

--- 7.1 Intervals/1_InsertInterval.py
+++ 7.1 Intervals/1_InsertInterval.py
@@ -45,3 +45,25 @@ class Solution:
                 ans.append(intervals[i])
         return ans
 
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        ans = []
+        n = len(intervals)
+
+        i = 0
+        # add all intervals before newInterval to ans
+        while i < n and intervals[i][1] < newInterval[0]:
+            ans.append(intervals[i])
+            i += 1
+
+        # merge overlapping intervals with newIntervals
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        # add the merged newIntervals to ans | add rest of intervals after newIntervals
+        ans.append(newInterval)
+        ans.extend(intervals[i:])
+        return ans
