48cfbfd293ad23fa5c5fbf5c9cdf178c8728f027
Hieu <hieuhho.dev@gmail.com>
2025-10-24 13:13:57 -0700

    solved 6.1.7

--- 6.1 Heap (Priority Queue)/7_FindMedianFromDataStream.py
+++ 6.1 Heap (Priority Queue)/7_FindMedianFromDataStream.py
@@ -35,13 +35,35 @@
 #     -100,000 <= num <= 100,000
 #     findMedian will only be called after adding at least one integer to the data structure.
 
+import heapq
+
 class MedianFinder:
 
     def __init__(self):
-
+        self.left_half = []       # max heap, stores min -> mid | use neg num so [0]
+        self.right_half = []       # min heap stores mid -> max | use mid
 
     def addNum(self, num: int) -> None:
+        middle = self.right_half[0] if self.right_half else float("inf")
+        if middle < num:
+            heapq.heappush(self.right_half, num)
+        else:
+            heapq.heappush(self.left_half, -num)
+
+        if len(self.left_half) > len(self.right_half)  + 1:
+            # "left is longer"
+            heapq.heappush(self.right_half, -heapq.heappop(self.left_half))
+        elif len(self.right_half) > len(self.left_half)  + 1:
+            # "right is longer"
+            heapq.heappush(self.left_half, -heapq.heappop(self.right_half))
 
 
     def findMedian(self) -> float:
+        len_diff = len(self.left_half) - len(self.right_half)
+
+        if len(self.left_half) > len(self.right_half):
+            return -self.left_half[0]
+        if len(self.right_half) > len(self.left_half):
+            return self.right_half[0]
+        return (-self.left_half[0] + self.right_half[0]) / 2
 
