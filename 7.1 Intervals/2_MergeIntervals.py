fa57a245d88367ea97d475baf4fc6c39929ddb4a
Hieu <hieuhho.dev@gmail.com>
2025-10-27 11:41:44 -0700

    solved 7.1.2

--- 7.1 Intervals/2_MergeIntervals.py
+++ 7.1 Intervals/2_MergeIntervals.py
@@ -23,3 +23,20 @@
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        ans = []
+        n = len(intervals)
+        i = 1
+        ans.append(intervals[0])
+        while i < n:
+            start1, end1 = ans[-1]
+            start2, end2 = intervals[i]
+
+            if start2 <= end1:
+                new_start = min(start1, start2)
+                new_end = max(end1, end2)
+                ans[-1] = [new_start, new_end]
+            else:
+                ans.append([start2, end2])
+            i += 1
+        return ans
