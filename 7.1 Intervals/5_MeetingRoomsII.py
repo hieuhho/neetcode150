f1d6c2013955476b196f62b945d8ddfc94f81068
Hieu <hieuhho.dev@gmail.com>
2025-10-27 13:29:31 -0700

    solved 7.1.5

--- 7.1 Intervals/5_MeetingRoomsII.py
+++ 7.1 Intervals/5_MeetingRoomsII.py
@@ -29,5 +29,18 @@ class Interval(object):
         self.end = end
 """
 
+import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        if len(intervals) == 1:
+            return 1
+        intervals.sort(key=lambda x:x.start)
+        ans = [intervals[0].end]
+
+        for i in range(1, len(intervals)):
+            if intervals[i].start >= ans[0]:
+                heapq.heappop(ans)
+            heapq.heappush(ans, intervals[i].end)
+        return len(ans)
