e1df92d6a6c8deecce1dfc6702587d96fafafa2f
Hieu <hieuhho.dev@gmail.com>
2025-11-04 11:37:08 -0800

    solved 5.2.4

--- 5.2 Backtracking/4_Permutations.py
+++ 5.2 Backtracking/4_Permutations.py
@@ -18,3 +18,16 @@
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(i, path):
+            if i == len(nums):
+                res.append(path.copy())
+                return
+
+            for idx in range(i, len(path)):
+                nums[i], nums[idx] = nums[idx], nums[i]
+                backtrack(i + 1, path)
+                nums[idx], nums[i] = nums[i], nums[idx]
+        backtrack(0, nums)
+        return res
