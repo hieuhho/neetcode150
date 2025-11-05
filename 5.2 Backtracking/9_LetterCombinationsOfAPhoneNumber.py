5d0f01283c3f61a4f2e0f5ef0b8e564e83cc3d6f
Hieu <hieuhho.dev@gmail.com>
2025-11-05 12:05:59 -0800

    solved 5.2.9

--- 5.2 Backtracking/9_LetterCombinationsOfAPhoneNumber.py
+++ 5.2 Backtracking/9_LetterCombinationsOfAPhoneNumber.py
@@ -27,3 +27,29 @@
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        phone = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        res = []
+
+        def backtrack(i, current):
+            if len(current) == len(digits):
+                res.append("".join(current))
+                return
+
+            for l in phone[digits[i]]:
+                current.append(l)
+                backtrack(i + 1, current)
+                current.pop()
+
+        if digits:
+            backtrack(0, [])
+        return res
