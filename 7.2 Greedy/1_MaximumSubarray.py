d731b2c8d438e43dfefca12d27c0b65a94bb9838
Hieu <hieuhho.dev@gmail.com>
2025-11-10 10:44:38 -0800

    solved 7.2.1

--- 7.2 Greedy/1_MaximumSubarray.py
+++ 7.2 Greedy/1_MaximumSubarray.py
@@ -25,3 +25,8 @@
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        current = max_sum = nums[0]
+        for i in range(1, len(nums)):
+            current = max(current + nums[i], nums[i])
+            max_sum = max(current, max_sum)
+        return max_sum
