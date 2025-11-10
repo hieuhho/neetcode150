25e3e80c206666cf62711b4c2e6e219741c3f3ca
Hieu <hieuhho.dev@gmail.com>
2025-11-10 15:54:56 -0800

    solved 7.2.7

--- 7.2 Greedy/7_PartitionLabels.py
+++ 7.2 Greedy/7_PartitionLabels.py
@@ -26,3 +26,16 @@
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        res = []
+        last_seen = {}
+        for i, v in enumerate(s):
+            last_seen[v] = i
+
+        size = end = 0
+        for i, v in enumerate(s):
+            size += 1
+            end = max(end, last_seen[v])
+            if end == i:
+                res.append(size)
+                size = 0
+        return res
