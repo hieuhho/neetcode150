044b7c52f5df979494a5de31c2849ec79d699d39
Hieu <hieuhho.dev@gmail.com>
2025-11-10 15:25:14 -0800

    solved 7.2.5

--- 7.2 Greedy/5_HandOfStraights.py
+++ 7.2 Greedy/5_HandOfStraights.py
@@ -30,3 +30,17 @@
 
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize: return False
+        freq = {}
+        for card in hand:
+            freq[card] = freq.get(card, 0) + 1
+
+        hand.sort()
+
+        for card in hand:
+            if freq[card]:
+                for i in range(card, card + groupSize):
+                    if not freq.get(i):
+                        return False
+                    freq[i] -= 1
+        return True
