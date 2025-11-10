baeb3b4a7408df0d7a46cec6702ab9440d9cb259
Hieu <hieuhho.dev@gmail.com>
2025-11-10 15:37:38 -0800

    solved 7.2.6

--- 7.2 Greedy/6_MergeTripletsToFormTarget.py
+++ 7.2 Greedy/6_MergeTripletsToFormTarget.py
@@ -31,3 +31,15 @@
 
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        found = set()
+        for triplet in triplets:
+            if any([triplet[0] > target[0],
+                triplet[1] > target[1],
+                triplet[2] > target[2]]
+            ):
+                continue
+
+            for i, num in enumerate(triplet):
+                if num == target[i]:
+                    found.add(i)
+        return len(found) == len(target)
