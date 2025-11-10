3154aeb837f6af13e41d6c58077ba9998c1f5bc5
Hieu <hieuhho.dev@gmail.com>
2025-11-10 13:03:30 -0800

    solved 7.2.3

--- 7.2 Greedy/3_JumpGameII.py
+++ 7.2 Greedy/3_JumpGameII.py
@@ -30,3 +30,14 @@
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        res = 0
+        l, r = 0, 0
+        while r < len(nums) - 1:
+            farthest = r
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+
+            res += 1
+            l = r + 1
+            r = farthest
+        return res
