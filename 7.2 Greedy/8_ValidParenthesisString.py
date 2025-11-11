5a8a1f21f260ed0d9b38d40de1e8ac889809a127
Hieu <hieuhho.dev@gmail.com>
2025-11-10 16:07:47 -0800

    solved 7.2.8

--- 7.2 Greedy/8_ValidParenthesisString.py
+++ 7.2 Greedy/8_ValidParenthesisString.py
@@ -34,3 +34,33 @@
 
 class Solution:
     def checkValidString(self, s: str) -> bool:
+        left = []
+        star = []
+
+        for i, p in enumerate(s):
+            # add ( and * to stack
+            if p == "(":
+                left.append(i)
+            elif p == "*":
+                star.append(i)
+            else:
+                # when encountering ):
+                # if there is not open or *: is auto invalid
+                if not left and not star:
+                    return False
+                # else pop a left to close ()
+                if left:
+                    left.pop()
+                # else pop a * to close ()
+                else:
+                    star.pop()
+                # this should close out all (), ) should no longer exists
+
+        # if there's any remaining
+        # the star is then = ), and should appear AFTER (
+        while left and star:
+            if left.pop() > star.pop():
+                return False
+
+        # left should be empty
+        return not left
